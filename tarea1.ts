6284b8a3088ece3239750f318090c9a17ff53167
RRosalia22 <91857363+RRosalia22@users.noreply.github.com>
2023-09-16 11:13:39 -0400

    Update tarea1.ts

--- tarea1.ts
+++ tarea1.ts
@@ -1,98 +1,113 @@
-class Book{
-    constructor(public title: string, public author: string, public isLoaded: boolean = false, public isAvailable: string) {}
+class Book {
+    private dueDate: Date | undefined; // Nueva propiedad para la fecha de devolución
+    private returnDate: Date | undefined; // Nueva propiedad para la fecha de devolución real
+
+    constructor(public title: string, public author: string, public isLoaded: boolean = false) {}
+
+    // Método para registrar la fecha de préstamo
+    borrowBook() {
+        if (!this.dueDate) {
+            const currentDate = new Date();
+            this.dueDate = new Date(currentDate.setDate(currentDate.getDate() + 7)); // 7 días de préstamo estándar
+        }
+    }
+
+    // Método para calcular si el libro está vencido y generar multa
+    calculateFine(): number {
+        if (!this.returnDate || !this.dueDate) {
+            return 0; // No hay fecha de devolución o fecha de préstamo
+        }
+
+        const daysLate = Math.max(0, Math.ceil((this.returnDate.getTime() - this.dueDate.getTime()) / (1000 * 60 * 60 * 24)));
+        return daysLate * 1; // $1 de multa por cada día de retraso
+    }
+
+    // Método para devolver el libro
+    returnBook() {
+        this.returnDate = new Date();
+    }
 }
-class User{
+
+class User {
     constructor(public userID: string, public name: string) {}
 }
-interface ILoadManager{
-    loadBook(book:Book, user: User);
-    returnBook(book:Book, user: User):void;
+
+interface ILogger {
+    log(message: string): void;
 }
 
-interface Ilogger{
-    log(message:string):void;
-}
-
-//
-interface Infologger{
-    log(message:string):void;
-}
-interface Warninglogger{
-    log(message:string):void;
-}
-interface Errorlogger{
-    log(message:string):void;
-}
-
-
-class ConsoleLogger implements Ilogger, Infologger, Warninglogger,Errorlogger {
-    log(message:string):void{
+class ConsoleLogger implements ILogger {
+    log(message: string): void {
         console.log(message);
     }
 }
 
-// un loger escribe 
-//inyectando una dependendcia a nuestra clase 
-// crear interfaces para logs de error warning e info
-// crear un metodo con una logica si un libro tiene un titulo correcto
-//si no lo tiene mostrar menseja ed eerror
-// si no lo tiene mostrar un mensaje de error en log y si tiene el mismo nombre 
-// mostrar un mensaje de info
-// modificar el metodo find para que retorne un warning cuando no encuentres el libro
+interface ILoadManager {
+    loadBook(book: Book, user: User): void;
+    returnBook(book: Book, user: User): void;
+}
 
-class Library implements ILoadManager, Infologger,Warninglogger,Errorlogger{
-    
-    private books: Book[]=[];
-    private loadBooks: Map<string,User>=new Map();
-    constructor(private logger: Ilogger, private loggerE: Errorlogger, private loggerI: Infologger, private loggerW: Warninglogger) {}
+interface ILoggable {
+    log(message: string): void;
+}
+
+class Library implements ILoadManager, ILoggable {
+    private books: Book[] = [];
+    private loadBooks: Map<string, User> = new Map();
+
+    constructor(
+        private logger: ILogger,
+        private errorLogger: ILogger,
+        private infoLogger: ILogger,
+        private warningLogger: ILogger
+    ) {}
+
     log(message: string): void {
-        throw new Error("Method not implemented.");
+        this.logger.log(message);
     }
-    
+
     loadBook(book: Book, user: User) {
-        if(book.isLoaded){
-            this.logger.log('Book is loaded');
-            return
+        if (book.isLoaded) {
+            this.errorLogger.log('Book is already loaded');
+            return;
         }
-        this.loadBooks.set(book.isAvailable,user);
-        book.isLoaded=true;
-        this.loggerI.log(`${user.name} (id: ${user.userID} ) has borrowed ${book.title}.`);
-        
+        book.borrowBook(); // Registrar la fecha de préstamo
+        this.loadBooks.set(book.title, user);
+        book.isLoaded = true;
+        this.infoLogger.log(`${user.name} (id: ${user.userID}) has borrowed ${book.title}.`);
     }
-    returnBook(book: Book, user: User):void {
-        if(!book.isLoaded){
-            this.logger.log('Book is not loaded');
-            return
-        }
-        this.loadBooks.delete(book.isAvailable);
-        book.isLoaded=false;
-        this.loggerI.log(`${book.title} has been returned for ${user.name} (id: ${user.userID} )`);
-    }
-    // mostrar en un log de inf0 toda la informacion dek usuario que se esata prestando un libro
-    // mostrar en un log info toda la info del usuario que esta devolviendo el libro
-    // debe mostrar el objeto completp 
 
-    addBook(book:Book){
-        this.logger.log('inicio de operación');
+    returnBook(book: Book, user: User): void {
+        if (!book.isLoaded) {
+            this.errorLogger.log('Book is not loaded');
+            return;
+        }
+
+        const borrowedUser = this.loadBooks.get(book.title);
+        if (borrowedUser && borrowedUser.userID === user.userID) {
+            book.returnBook(); // Registrar la fecha de devolución real
+            const fineAmount = book.calculateFine(); // Calcular multa
+            if (fineAmount > 0) {
+                this.errorLogger.log(`Book returned late. ${fineAmount} dollar(s) fine generated.`);
+            }
+            this.loadBooks.delete(book.title);
+            book.isLoaded = false;
+            this.infoLogger.log(`${book.title} has been returned by ${user.name} (id: ${user.userID})`);
+        } else {
+            this.errorLogger.log(`Book can only be returned by the borrower (${user.name})`);
+        }
+    }
+
+    addBook(book: Book) {
+        this.logger.log('Adding a book...');
         this.books.push(book);
-        this.logger.log('fin de operación');
+        this.logger.log(`Book "${book.title}" added successfully.`);
     }
 
-    findBookByTitle(title: string) : Book | undefined{
-        const foundBook = this.books.find(book => book.title === title);
-        if(!foundBook){
-            this.loggerW.log("Warning: Book not found")
-        }
-        return foundBook;
-    }
-
-    CheckTitle(title: string) : Book | undefined {
-        const foundBook = this.books.find(book => book.title === title);
-        if(!foundBook){
-            this.loggerE.log('Error: El titulo del libro no es igual a la entrada')
-        }
-        else{
-            this.loggerI.log('Libro tiene el mismo titulo')
+    findBookByTitle(title: string): Book | undefined {
+        const foundBook = this.books.find((book) => book.title === title);
+        if (!foundBook) {
+            this.warningLogger.log("Book not found");
         }
         return foundBook;
     }
